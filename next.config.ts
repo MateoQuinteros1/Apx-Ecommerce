71c2b4420b579bf7764790dffe81962eac71bc3e
Mateo <mateoquinteros11@gmail.com>
2026-02-25 08:46:19 -0300

    Next config modificado (intento de solucionar error)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ["sequelize", "sequelize-typescript", "pg"],
+  serverExternalPackages: [
+    "sequelize",
+    "sequelize-typescript",
+    "pg",
+    "pg-hstore",
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [
+        ...(Array.isArray(config.externals) ? config.externals : []),
+        "pg",
+        "pg-hstore",
+        "pg-native",
+        "sequelize",
+        "sequelize-typescript",
+      ];
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
