d40cde79d81d1db1f9c1b6fe088e2d07d3ceb9c1
Mateo <mateoquinteros11@gmail.com>
2026-03-18 17:37:20 -0300

    Proxy file edited

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const allowedOrigins = ["http://localhost:3000"];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const origin = request.headers.get("origin") ?? "";
   const isAllowed = allowedOrigins.includes(origin);
 
